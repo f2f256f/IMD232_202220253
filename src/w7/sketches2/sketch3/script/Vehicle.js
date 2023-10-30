d1356d2ea8f5721a5149baa1ff25a478e6cbf57c
”f2f2562” <126849568+f2f256f@users.noreply.github.com>
2023-10-30 16:01:25 +0900

    w8수업 끝

---
+++ src/w7/sketches2/sketch3/script/Vehicle.js
@@ -0,0 +1,131 @@
+class Vehicle {
+  constructor(x, y, mass, rad, speedMx, forceMx, color) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.acc = createVector();
+    this.mass = mass;
+    this.rad = rad;
+    this.speedMx = speedMx;
+    this.forceMx = forceMx;
+    this.neighborhooodRad = 50;
+    this.color = color;
+  }
+
+  cohesion(others) {
+    let cnt = 0;
+    const steer = createVector(0, 0);
+    others.forEach((each) => {
+      if (each !== this) {
+        const distSq =
+          (this.pos.x - each.pos.x) ** 2 + (this.pos.y - each.pos.y) ** 2;
+        if (distSq < this.neighborhooodRad ** 2) {
+          steer.add(each.pos);
+          cnt++;
+        }
+      }
+    });
+    if (cnt > 0) {
+      steer.div(cnt);
+      steer.sub(this.pos);
+      steer.setMag(this.speedMx);
+      steer.sub(this.vel);
+      steer.limit(this.forceMx);
+    }
+    return steer;
+  }
+
+  align(others) {
+    let cnt = 0;
+    const steer = createVector(0, 0);
+    others.forEach((each) => {
+      if (each !== this) {
+        const distSq = //거리의 제곱 가져오기
+          (this.pos.x - each.pos.x) ** 2 + (this.pos.y - each.pos.y) ** 2;
+        if (distSq < this.neighborhooodRad ** 2) {
+          steer.add(each.vel); //더한 만큼 나누기 = 각도 가져오기
+          //   steer.add(p5.Vector.normalize(each.vel));
+          cnt++;
+        }
+      }
+    });
+    if (cnt > 0) {
+      //느린 친구들은 빨라지게
+      steer.div(cnt);
+      steer.setMag(this.speedMx);
+      steer.sub(this.vel);
+      steer.limit(this.forceMx);
+    }
+    return steer;
+  }
+
+  separate(others) {
+    let cnt = 0;
+    const steer = createVector(0, 0);
+    others.forEach((each) => {
+      if (each !== this) {
+        //내가 아닐 때에만 계산
+        const dist = this.pos.dist(each.pos);
+        if (dist > 0 && this.rad + each.rad > dist) {
+          //0이 되지 않게 두 가지 조건 설정
+          const distNormal = dist / (this.rad + each.rad);
+          const towardMeVec = p5.Vector.sub(this.pos, each.pos);
+          towardMeVec.setMag(1 / distNormal); //반비례
+          steer.add(towardMeVec);
+          cnt++;
+        }
+      }
+    });
+    if (cnt > 0) {
+      steer.div(cnt);
+      steer.setMag(this.speedMx);
+      steer.sub(this.vel);
+      steer.limit(this.forceMx);
+    }
+    return steer;
+  }
+
+  applyForce(force) {
+    const forceDivedByMass = p5.Vector.div(force, this.mass);
+    this.acc.add(forceDivedByMass);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.speedMx);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  borderInfinite() {
+    if (this.pos.x < -infiniteOffset) {
+      this.pos.x = width + infiniteOffset;
+    } else if (this.pos.x > width + infiniteOffset) {
+      this.pos.x = -infiniteOffset;
+    }
+    if (this.pos.y < -infiniteOffset) {
+      this.pos.y = height + infiniteOffset;
+    } else if (this.pos.y > height + infiniteOffset) {
+      this.pos.y = -infiniteOffset;
+    }
+  } //밖으로 나간 것들 다시 안으로 들어오게
+
+  display() {
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading()); //heading > vel 방향으로 움직임
+    noStroke();
+    fill(this.color);
+    beginShape();
+    vertex(this.rad, 0);
+    vertex(this.rad * cos(radians(-135)), this.rad * sin(radians(-135)));
+    vertex(0, 0);
+    vertex(this.rad * cos(radians(135)), this.rad * sin(radians(135)));
+    endShape(CLOSE);
+    // noFill();
+    // stroke(0, 0, 60);
+    // ellipse(0, 0, 2 * this.rad);
+    // stroke(0, 0, 80);
+    // ellipse(0, 0, 2 * this.neighborhooodRad);
+    pop();
+  }
+}
