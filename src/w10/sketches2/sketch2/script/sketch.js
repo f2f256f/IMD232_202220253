97913d4b85a77e80c1d52f29770db37537348211
”f2f2562” <126849568+f2f256f@users.noreply.github.com>
2023-11-13 16:00:02 +0900

    w10 수업

--- src/w10/sketches2/sketch2/script/sketch.js
+++ src/w10/sketches2/sketch2/script/sketch.js
@@ -1,111 +1,51 @@
-var Engine = Matter.Engine,
-  Render = Matter.Render,
-  Runner = Matter.Runner,
-  Composites = Matter.Composites,
-  Events = Matter.Events,
-  Constraint = Matter.Constraint,
-  MouseConstraint = Matter.MouseConstraint,
-  Mouse = Matter.Mouse,
-  Body = Matter.Body,
-  Composite = Matter.Composite,
-  Bodies = Matter.Bodies;
+let cells = [];
 
-// create engine
-var engine = Engine.create(),
-  world = engine.world;
+const colNum = 100,
+  rowNum = colNum;
 
-// create renderer
-let elemTarget = document.querySelector('#canvas');
-var render = Render.create({
-  element: elemTarget,
-  engine: engine,
-  options: {
-    width: 800,
-    height: 600,
-    showAngleIndicator: true,
-  },
-});
+let w, h;
 
-Render.run(render);
+function setup() {
+  setCanvasContainer('canvas', 1, 1, true);
 
-// create runner
-var runner = Runner.create();
-Runner.run(runner, engine);
+  randomSeed(1);
 
-// add bodies
-var ground = Bodies.rectangle(395, 600, 815, 50, {
-    isStatic: true,
-    render: { fillStyle: '#060a19' },
-  }),
-  rockOptions = { density: 0.004 },
-  rock = Bodies.polygon(170, 450, 8, 20, rockOptions),
-  anchor = { x: 170, y: 450 },
-  elastic = Constraint.create({
-    pointA: anchor,
-    bodyB: rock,
-    length: 0.01,
-    damping: 0.01,
-    stiffness: 0.05,
-  });
+  w = width / colNum;
+  h = height / rowNum;
 
-var pyramid = Composites.pyramid(500, 300, 9, 10, 0, 0, function (x, y) {
-  return Bodies.rectangle(x, y, 25, 40);
-});
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const x = w * col;
+      const y = h * row;
+      const state = random() < 0.5;
+      const idx = colNum * row + col;
+      const newCell = new Cell(x, y, w, h, state, idx);
 
-var ground2 = Bodies.rectangle(610, 250, 200, 20, {
-  isStatic: true,
-  render: { fillStyle: '#060a19' },
-});
-
-var pyramid2 = Composites.pyramid(550, 0, 5, 10, 0, 0, function (x, y) {
-  return Bodies.rectangle(x, y, 25, 40);
-});
-
-Composite.add(engine.world, [
-  ground,
-  pyramid,
-  ground2,
-  pyramid2,
-  rock,
-  elastic,
-]);
-
-Events.on(engine, 'afterUpdate', function () {
-  if (
-    mouseConstraint.mouse.button === -1 &&
-    (rock.position.x > 190 || rock.position.y < 430)
-  ) {
-    // Limit maximum speed of current rock.
-    if (Body.getSpeed(rock) > 45) {
-      Body.setSpeed(rock, 45);
+      cells.push(newCell);
     }
-
-    // Release current rock and add a new one.
-    rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
-    Composite.add(engine.world, rock);
-    elastic.bodyB = rock;
   }
-});
 
-// add mouse control
-var mouse = Mouse.create(render.canvas),
-  mouseConstraint = MouseConstraint.create(engine, {
-    mouse: mouse,
-    constraint: {
-      stiffness: 0.2,
-      render: {
-        visible: false,
-      },
-    },
+  cells.forEach((eachCell) => {
+    eachCell.addFriends(cells);
   });
 
-Composite.add(world, mouseConstraint);
+  console.log(cells);
 
-// keep the mouse in sync with rendering
-render.mouse = mouse;
+  background('white');
+}
 
-// fit the render viewport to the scene
-Render.lookAt(render, {
-  min: { x: 0, y: 0 },
-  max: { x: 800, y: 600 },
-});
+function draw() {
+  background('white');
+
+  cells.forEach((eachCell) => {
+    eachCell.calcNextState();
+  });
+
+  cells.forEach((eachCell) => {
+    eachCell.updateState();
+  });
+
+  cells.forEach((eachCell) => {
+    eachCell.display();
+  });
+}
